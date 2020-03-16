e0deb80ecd542928ef2a44b1c07da7a7aacdf5e0
Maksim Kirilko <maksimkirilko@gmail.com>
2020-03-16 20:09:24 +0300

    feat: switch portfolio projects

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
     window.addEventListener("hashchange", onHashChanged, false);
 
     function domReady(event) {
-        changeActiveNavItem(event.target.location.hash.slice(1))
+        changeActiveNavItem(event.target.location.hash.slice(1));
+
+        document.querySelector('.tags-container').addEventListener('click', onPortfolioTagClicked);
     }
 
     function onHashChanged(event) {
@@ -20,5 +22,52 @@
             link.parentNode.classList.add('active');
         }
     }
+
+    function onPortfolioTagClicked(event) {
+        if (!event.target.classList.contains('tag')) {
+            return;
+        }
+        const active = document.querySelector('.tag.active');
+        if (active) {
+            active.classList.remove('active');
+        }
+        event.target.classList.add('active');
+        const images = document.querySelectorAll('.gallery > .picture');
+        const value = getImagesRow(event.target.classList);
+        for (let i = 0; i < images.length; i++) {
+            if (value === 1) {
+                if (i > 3) {
+                    images[i].style.display = 'none';
+                } else {
+                    images[i].style.display = 'block';
+                }
+            } else if (value === 2) {
+                if (i < 4 || i > 7) {
+                    images[i].style.display = 'none';
+                } else {
+                    images[i].style.display = 'block';
+                }
+            } else if (value === 3) {
+                if (i < 8) {
+                    images[i].style.display = 'none';
+                } else {
+                    images[i].style.display = 'block';
+                }
+            } else {
+                images[i].style.display = 'block';
+            }
+        }
+    }
+
+    function getImagesRow(classList) {
+        return classList.contains('web-design')
+                ? 1
+                : classList.contains('graphic-design')
+                ? 2
+                : classList.contains('artwork')
+                ? 3
+                : 0;
+    }
+
 
 })();
